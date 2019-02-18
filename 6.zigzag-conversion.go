9bcbaff046a4b1ae4f3e478f58060a1e5f203f92
denghuaguang <denghuaguang@onething.net>
2019-02-18 21:01:47 +0800

    add 6

---
+++ 6.zigzag-conversion.go
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode id=6 lang=golang
+ *
+ * [6] ZigZag Conversion
+ *
+ * https://leetcode.com/problems/zigzag-conversion/description/
+ *
+ * algorithms
+ * Medium (30.48%)
+ * Total Accepted:    283.2K
+ * Total Submissions: 929.1K
+ * Testcase Example:  '"PAYPALISHIRING"\n3'
+ *
+ * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number
+ * of rows like this: (you may want to display this pattern in a fixed font for
+ * better legibility)
+ *
+ *
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ *
+ *
+ * And then read line by line: "PAHNAPLSIIGYIR"
+ *
+ * Write the code that will take a string and make this conversion given a
+ * number of rows:
+ *
+ *
+ * string convert(string s, int numRows);
+ *
+ * Example 1:
+ *
+ *
+ * Input: s = "PAYPALISHIRING", numRows = 3
+ * Output: "PAHNAPLSIIGYIR"
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: s = "PAYPALISHIRING", numRows = 4
+ * Output: "PINALSIGYAHRPI"
+ * Explanation:
+ *
+ * P     I    N
+ * A   L S  I G
+ * Y A   H R
+ * P     I
+ *
+ */
+func convert(s string, numRows int) string {
+	if len(s) == 0 {
+		return s
+	}
+	var array [numRows][len(s)]byte
+	var i, j int
+	for i < numRows; j < len(s); {
+
+	}
+
+}
